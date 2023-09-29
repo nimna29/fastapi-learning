ca6a999ae101e948cba3bd2777601c4527be5908
Nimna Niwarthana <niwarthananimna@gmail.com>
2023-09-30 02:31:39 +0530

    Added API Integration with ML Model for Predictions

--- ml-with-fastapi/app/BankNotes.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-# Create object with feature names 
-# ['variance', 'skewness', 'curtosis', 'entropy']
-class BankNote(BaseModel):
-    variance: float 
-    skewness: float 
-    curtosis: float 
-    entropy: float
--- ml-with-fastapi/app/app.py
+++ ml-with-fastapi/app/app.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-# from BankNotes import BankNote
+from pydantic import BaseModel
 import numpy as np
 import pickle
 import pandas as pd
 
 app = FastAPI(
-    title="FastAPI With Simple ML Model",
+    title="FastAPI with Simple ML Model",
     description="ML Model Integration with FastAPI",
     version="0.0.0"
 )
@@ -14,7 +14,35 @@ app = FastAPI(
 pickle_in = open("app/ml_model.pkl", "rb")
 classifier=pickle.load(pickle_in)
 
+# Create object with feature names 
+# ['variance', 'skewness', 'curtosis', 'entropy']
+class BankNotes(BaseModel):
+    variance: float 
+    skewness: float 
+    curtosis: float 
+    entropy: float
+
+
 # Root rount
 @app.get("/", tags=['Root'])
 def home():
     return {'message': 'Welcome to the Bank Note Authentication ML App'}
+
+
+# Expose the prediction functionality:
+# Make a presdiction from the pass JSON date and return the predicted Bank Note with Confidence value
+@app.post("/predict", tags=['Prediction'])
+def predict_bank_note(data:BankNotes):
+    data = data.model_dump()
+    variance=data['variance']
+    skewness=data['skewness']
+    curtosis=data['curtosis']
+    entropy=data['entropy']
+    prediction = classifier.predict([[variance,skewness,curtosis,entropy]])
+    if(prediction[0]>0.5):
+        prediction="Fake note"
+    else:
+        prediction="Its a Bank note"
+    return {
+        'prediction': prediction
+    }
